46781c0e1e3702577bab32643ac72c9050805248
mi1acl <148514875+mi1acl@users.noreply.github.com>
2023-11-29 00:33:18 +0000

    Chapter 1

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,19 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle r1 = new Rectangle(10, 20);
+Rectangle r2 = new Rectangle(5);
+Rectangle r3 = new Rectangle(20, 30);
 
+Console.WriteLine($"r1 area is: {r1.Area()}");
+Console.WriteLine($"r2 area is: {r2.Area()}");
+Console.WriteLine($"r3 area is: {r3.Area()}");
 
 // TODO: Change the values of width and height
+r1.Width = 4;
+r1.Height = 6;
+Console.WriteLine($"r1 area is: {r1.Area()}");
+
+r1.BorderSize = 10;
+Console.WriteLine($"r1 border size is: {r1.BorderSize}");
+
--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -3,14 +3,90 @@
 
 // TODO: Define a class using the "class" keyword
 
-    // TODO: The constructor accepts parameters used to create the object
+// TODO: The constructor accepts parameters used to create the object
 
 
-    // TODO: For convenience, we can have a constructor that takes one value
+// TODO: For convenience, we can have a constructor that takes one value
+// for squares that have the same side size
+
+
+// TODO: Classes can define methods that return values
+
+
+// TODO: member variables hold data
+// Define a class using the "class" keyword
+public class Rectangle
+{
+    // The constructor accepts parameters used to create the object
+    public Rectangle(int w, int h)
+    {
+        Width = w;
+        Height = h;
+    }
+    // For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
+    public Rectangle(int side)
+    {
+        Width = side;
+        Height = side;
+    }
 
+    // Classes can define methods that return values
+    public int Area()
+    {
+        return Width * Height;
+    }
 
-    // TODO: Classes can define methods that return values
+    // member variables hold data
+    int width;
+    int height;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Width cannot be negative");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Height cannot be negative");
+            }
+            else
+            {
+                height = value;
+            }
+        }
 
+    }
+    public int Perimeter()
+    {
+        return 2 * (Width + Height);
+    }
+    public int BorderSize { get; set; } = 1;
+}
 
-    // TODO: member variables hold data
+// Define a Square class that inherits from Rectangle
+public class Square : Rectangle
+{
+    // The Square class inherits the Rectangle constructor
+    public Square(int side) : base(side)
+    {
+    }
+    // The Square class inherits the Rectangle Area() method
+}
